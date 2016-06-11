1cf4cc9d085a31b3e506695af4c3c5fd16f01b2d
Christopher Perry <chris@SanguineVM>
2016-06-11 16:25:26 +0800

    Added a script for a Debian VM.

---
+++ scripts/debbie.sh
@@ -0,0 +1,8 @@
+# apt-get update -y
+# apt-get upgrade -y
+# apt-get install build-essential module-assistant -y
+m-a prepare
+read -p
+sudo mount /media/cdrom
+mount /media/cdrom
+sh /media/cdrom/VBoxLinuxAdditions.run
