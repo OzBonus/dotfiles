c3cb7baab2bb1778ec8c742a32fc2fdaa4c97ade
CPerry <ozbonus@gmail.com>
2018-03-21 07:18:37 +0000

    Make user owner of ~/.jupyter

--- fresh-install/lightsail-jupyter.sh
+++ fresh-install/lightsail-jupyter.sh
@@ -54,6 +54,9 @@ c.NotebookApp.certfile = '/home/$SUDO_USER/.jupyter/keys/mycert.pem'
 END
 pyenv deactivate
 
+# Make sure user has the permission to access ~/.jupyer.
+chown $SUDO_USER: /home/$SUDO_USER/.jupyter/
+
 # Create a startup script and make it executable.
 cat <<END >> ~/jupyterlab.sh
 #!/bin/bash
